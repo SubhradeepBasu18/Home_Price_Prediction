7f69ea55c48970e8815696d89ec9ab96cd7dc612
Subhradeep <subhradeepbasu2002@gmail.com>
2024-09-02 02:52:00 +0530

    Updated UI

--- client/app.js
+++ client/app.js
@@ -1,24 +1,14 @@
-function getBathValue(){
-    var bath = document.getElementById('uiBathrooms');
-    for(var i in bath){
-        if(bath[i].checked){
-            return bath[i].value;
-        }
-    }
-    return -1; // Invalid Value
+function getBathValue() {
+    var bath = document.getElementById('radio-bath-1');
+    return bath.value !== "" ? bath.value : -1; // Return selected value or -1 if invalid
 }
 
-function getBHKValue(){
-    var bhk = document.getElementById('uiBHK');
-    for(var i in bhk){
-        if(bhk[i].checked){
-            return bhk[i].value;
-        }
-    }
-    return -1; // Invalid Value
+function getBHKValue() {
+    var bhk = document.getElementById('radio-bhk-1');
+    return bhk.value !== "" ? bhk.value : -1; // Return selected value or -1 if invalid
 }
 
-function onClickedEstimatePrice(){
+function onClickedEstimatePrice() {
     console.log("Estimate price button clicked");
     var sqft = document.getElementById("uiSqft");
     var bhk = getBHKValue();
@@ -26,39 +16,35 @@ function onClickedEstimatePrice(){
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
 
-    const url = 'http://127.0.0.1:5000/predict_home_price'
+    const url = 'http://127.0.0.1:5000/predict_home_price';
 
-    $.post(url,{
+    $.post(url, {
         total_sqft: parseFloat(sqft.value),
         bhk: bhk,
         bath: bath,
         location: location.value
-    },(data,status)=>{
+    }, function (data, status) {
         console.log(data.estimated_price);
         estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
         console.log(status);
-        
-    })
-    
+    });
 }
 
-function onPageLoad(){
+function onPageLoad() {
     console.log("document loaded");
-    var url = 'http://127.0.0.1:5000/get_location_names'
-    $.get(url,function(data,status) {
+    var url = 'http://127.0.0.1:5000/get_location_names';
+    $.get(url, function (data, status) {
         console.log("got response for get_location_names request");
 
-        if(data){
+        if (data) {
             var locations = data.locations;
             var uiLocations = document.getElementById("uiLocations");
-            for(var i in locations){
+            for (var i in locations) {
                 var opt = new Option(locations[i]);
-                $('#uiLocations').append(opt);
+                uiLocations.appendChild(opt);
             }
         }
-        
-    })
-    
+    });
 }
 
 window.onload = onPageLoad;
